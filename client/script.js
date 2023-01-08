5d0dc97b0fe27153da4f2ca25b2d9834f01bf5c1
Yangyi Jung <studwing@hotmail.com>
2023-01-07 20:24:10 -0800

    stuck in _refresh

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ class Poll {
   constructor(root, title) {
     this.root = root;
     this.selected = sessionStorage.getItem('selected');
-    this.endpoint - 'http://localhost:3000/poll';
+    this.endpoint = 'http://localhost:3000/poll';
 
     this.root.insertAdjacentHTML(
       'afterbegin',
@@ -15,9 +15,30 @@ class Poll {
     const response = await fetch(this.endpoint);
     const data = await response.json();
 
+    console.log(data);
+
     this.root.querySelectorAll('.poll__option').forEach((option) => {
       option.remove();
     });
+
+    for (const option of data) {
+      const template = doccument.createElement('template');
+      const fragment = template.content;
+      template.innerHTML = `
+    <div class="poll__option">
+      <span class="poll__label">${option.label}</span>
+        <div class="poll__option-info">
+            <div class="poll__option-fill"></div>
+             <span class="poll__label">${option.percentage}%</span>
+        </div>
+    </div>`;
+
+      fragment.querySelector(
+        '.poll__option-fill'
+      ).style.width = `${option.percentage}%`;
+
+      this.root.appendChild(fragment);
+    }
   }
 }
 
